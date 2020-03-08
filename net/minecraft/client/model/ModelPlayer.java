6e0f751c27d7cf9d6018f026c6099f08650b4970
Ksajo_jest_chudy <34755589+anKordii@users.noreply.github.com>
2020-03-08 16:52:55 +0100

    Create ModelPlayer.java

---
+++ net/minecraft/client/model/ModelPlayer.java
@@ -0,0 +1,55 @@
+public class ModelPlayer extends ModelBiped
+{
+    //add under this `CosmeticWings cosmeticWings;`
+    public ModelRenderer bipedLeftArmwear;
+    public ModelRenderer bipedRightArmwear;
+    public ModelRenderer bipedLeftLegwear;
+    public ModelRenderer bipedRightLegwear;
+    public ModelRenderer bipedBodyWear;
+    private ModelRenderer bipedCape;
+    private ModelRenderer bipedDeadmau5Head;
+    private boolean smallArms;
+    /**
+    * HERE
+    */
+    CosmeticWings cosmeticWings;
+    
+    //In Render add `this.cosmeticWings.RenderWings(entityIn.isSneaking(), entityIn, 1);`
+    public void render(Entity entityIn, float p_78088_2_, float p_78088_3_, float p_78088_4_, float p_78088_5_, float p_78088_6_, float scale)
+    {
+        super.render(entityIn, p_78088_2_, p_78088_3_, p_78088_4_, p_78088_5_, p_78088_6_, scale);
+        GlStateManager.pushMatrix();
+
+        if (this.isChild)
+        {
+            float f = 2.0F;
+            GlStateManager.scale(1.0F / f, 1.0F / f, 1.0F / f);
+            GlStateManager.translate(0.0F, 24.0F * scale, 0.0F);
+            this.bipedLeftLegwear.render(scale);
+            this.bipedRightLegwear.render(scale);
+            this.bipedLeftArmwear.render(scale);
+            this.bipedRightArmwear.render(scale);
+            this.bipedBodyWear.render(scale);
+        }
+        else
+        {
+            if (entityIn.isSneaking())
+            {
+                GlStateManager.translate(0.0F, 0.2F, 0.0F);
+            }
+
+            this.bipedLeftLegwear.render(scale);
+            this.bipedRightLegwear.render(scale);
+            this.bipedLeftArmwear.render(scale);
+            this.bipedRightArmwear.render(scale);
+            this.bipedBodyWear.render(scale);
+        }
+
+        /**
+        * HERE
+        */
+        this.cosmeticWings.RenderWings(entityIn.isSneaking(), entityIn, 1);
+        
+        GlStateManager.popMatrix();
+    }
+ }
