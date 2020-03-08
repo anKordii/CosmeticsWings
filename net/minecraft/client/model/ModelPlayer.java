837b06f62220ed2cb47c62fbd2f99969fb90039b
Ksajo_jest_chudy <34755589+anKordii@users.noreply.github.com>
2020-03-08 19:50:51 +0100

    Update ModelPlayer.java

--- net/minecraft/client/model/ModelPlayer.java
+++ net/minecraft/client/model/ModelPlayer.java
@@ -13,7 +13,62 @@ public class ModelPlayer extends ModelBiped
     * 🌟HERE
     */
     CosmeticWings cosmeticWings;
-    
+    //🌟 In ModelPlayer add `this.cosmeticWings = new CosmeticWings();`
+    public ModelPlayer(float p_i46304_1_, boolean p_i46304_2_)
+    {
+        super(p_i46304_1_, 0.0F, 64, 64);
+        this.smallArms = p_i46304_2_;
+        this.bipedDeadmau5Head = new ModelRenderer(this, 24, 0);
+        this.bipedDeadmau5Head.addBox(-3.0F, -6.0F, -1.0F, 6, 6, 1, p_i46304_1_);
+        this.bipedCape = new ModelRenderer(this, 0, 0);
+        this.bipedCape.setTextureSize(64, 32);
+        this.bipedCape.addBox(-5.0F, 0.0F, -1.0F, 10, 16, 1, p_i46304_1_);
+
+        if (p_i46304_2_)
+        {
+            this.bipedLeftArm = new ModelRenderer(this, 32, 48);
+            this.bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 3, 12, 4, p_i46304_1_);
+            this.bipedLeftArm.setRotationPoint(5.0F, 2.5F, 0.0F);
+            this.bipedRightArm = new ModelRenderer(this, 40, 16);
+            this.bipedRightArm.addBox(-2.0F, -2.0F, -2.0F, 3, 12, 4, p_i46304_1_);
+            this.bipedRightArm.setRotationPoint(-5.0F, 2.5F, 0.0F);
+            this.bipedLeftArmwear = new ModelRenderer(this, 48, 48);
+            this.bipedLeftArmwear.addBox(-1.0F, -2.0F, -2.0F, 3, 12, 4, p_i46304_1_ + 0.25F);
+            this.bipedLeftArmwear.setRotationPoint(5.0F, 2.5F, 0.0F);
+            this.bipedRightArmwear = new ModelRenderer(this, 40, 32);
+            this.bipedRightArmwear.addBox(-2.0F, -2.0F, -2.0F, 3, 12, 4, p_i46304_1_ + 0.25F);
+            this.bipedRightArmwear.setRotationPoint(-5.0F, 2.5F, 10.0F);
+        }
+        else
+        {
+            this.bipedLeftArm = new ModelRenderer(this, 32, 48);
+            this.bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4, p_i46304_1_);
+            this.bipedLeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
+            this.bipedLeftArmwear = new ModelRenderer(this, 48, 48);
+            this.bipedLeftArmwear.addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4, p_i46304_1_ + 0.25F);
+            this.bipedLeftArmwear.setRotationPoint(5.0F, 2.0F, 0.0F);
+            this.bipedRightArmwear = new ModelRenderer(this, 40, 32);
+            this.bipedRightArmwear.addBox(-3.0F, -2.0F, -2.0F, 4, 12, 4, p_i46304_1_ + 0.25F);
+            this.bipedRightArmwear.setRotationPoint(-5.0F, 2.0F, 10.0F);
+        }
+
+        this.bipedLeftLeg = new ModelRenderer(this, 16, 48);
+        this.bipedLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, p_i46304_1_);
+        this.bipedLeftLeg.setRotationPoint(1.9F, 12.0F, 0.0F);
+        this.bipedLeftLegwear = new ModelRenderer(this, 0, 48);
+        this.bipedLeftLegwear.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, p_i46304_1_ + 0.25F);
+        this.bipedLeftLegwear.setRotationPoint(1.9F, 12.0F, 0.0F);
+        this.bipedRightLegwear = new ModelRenderer(this, 0, 32);
+        this.bipedRightLegwear.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, p_i46304_1_ + 0.25F);
+        this.bipedRightLegwear.setRotationPoint(-1.9F, 12.0F, 0.0F);
+        this.bipedBodyWear = new ModelRenderer(this, 16, 32);
+        this.bipedBodyWear.addBox(-4.0F, 0.0F, -2.0F, 8, 12, 4, p_i46304_1_ + 0.25F);
+        this.bipedBodyWear.setRotationPoint(0.0F, 0.0F, 0.0F);
+        /**
+        * 🌟HERE
+        */
+        this.cosmeticWings = new CosmeticWings();
+    }
     //🌟 In Render add `this.cosmeticWings.RenderWings(entityIn.isSneaking(), entityIn, 1);`
     public void render(Entity entityIn, float p_78088_2_, float p_78088_3_, float p_78088_4_, float p_78088_5_, float p_78088_6_, float scale)
     {
